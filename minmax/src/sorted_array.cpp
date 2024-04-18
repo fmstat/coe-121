5f2ca0ef85a7754fe925c4696fb7048d99d03b52
fmstat <stateresa.fm@gmail.com>
2024-04-18 14:43:48 +0800

    Update minmax files

---
+++ minmax/src/sorted_array.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    std::cout << "Minimum and maximum elements of array\n";
+    std::cout << "Custom implementation\n\n";
+
+    // Create array
+    const int SIZE = 5;
+    int nums[SIZE];
+
+    std::cout << "Array size: " << SIZE << "\n\n";
+
+    // Ask user for integers
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << "Enter an integer: ";
+        std::cin >> nums[i];
+    }
+
+    std::sort(nums, nums + SIZE);
+
+    // Display array
+    std::cout << "\n[";
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << nums[i];
+        if (i < SIZE - 1)
+            std::cout << ", ";
+    }
+    std::cout << "]\n\n";
+
+    std::cout << "Min: " << nums[0] << '\n';
+    std::cout << "Max: " << nums[SIZE - 1] << '\n';
+}
